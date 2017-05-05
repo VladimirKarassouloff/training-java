e03116ef0836b8f8a0fe2a8a73d43638329b1ce8
Vladimir Karassouloff <vkarassouloff@e-biz.fr>
2017-05-05 15:12:35 +0200

    [Conf] fix

--- projetmaven/scripts/dockermaven/maven.sh
+++ projetmaven/scripts/dockermaven/maven.sh
@@ -3,19 +3,11 @@ IMAGE="vkarassouloff/maven"
 NAME="maven"
 NETWORK="mynetwork"
 
+PATHPROJECT="/home/vkarassouloff/.jenkins/workspace/cdb/projetmaven"
+
 if [ "$(docker images -q $IMAGE 2> /dev/null)" = "" ]; then
     echo "pulling the image..."
     docker pull $IMAGE
 fi
 
-if [ "$(docker ps -a -f "name=$NAME" --format '{{.Names}}' 2> /dev/null)" = $NAME ]; then
-    if [ "$(docker inspect --format="{{.State.Running}}" $NAME 2> /dev/null)" = "false" ]; then
-        echo "Starting the container..." 
-        docker start $NAME
-    else
-        echo "Container already running..."
-    fi
-else
-    echo "Creating container..."
-    docker run --net=$NETWORK -d -it --rm --name=$NAME -v "/home/vkarassouloff/training-java/projetmaven:/usr/src/mymaven" -w /usr/src/mymaven $IMAGE mvn clean package
-fi
+docker run --net=$NETWORK -it --rm --name=$NAME -v "$PATHPROJECT:/usr/src/mymaven" -w /usr/src/mymaven $IMAGE mvn clean package
